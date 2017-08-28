b7d225f3b259be561fd8fa05e8f18d7359e9c87a
PeterKaminski09 <Peter.Kaminski09@gmail.com>
2017-08-28 14:28:35 -0700

    Blank app

---
+++ LA's BEST Robotics App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LA's BEST Robotics App
+//
+//  Created by Peter Kaminski on 8/28/17.
+//  Copyright © 2017 LA's BEST. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
