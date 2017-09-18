f2e00690591c37e3a46de8e4b6d4537165b73221
Mangoxylic <erickcho@usc.edu>
2017-09-18 16:31:56 -0700

    Added buttons and possible JSON formatting of them

--- LA's BEST Robotics App/ViewController.swift
+++ LA's BEST Robotics App/ViewController.swift
@@ -20,6 +20,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func mediumMotorButton(_ sender: UIButton) {
+        let json = JSON(["type": "Medium Motor", "power": 50, "rotations": 5, "brake": false])
+        let result = json.description
+        NSLog(result)
+        //let data = result.data(using: String.Encoding.utf8)!
+    }
+    
+    @IBAction func largeMotorButton(_ sender: UIButton) {
+        let json = JSON(["type": "Large Motor", "power": 50, "rotations": 5, "brake": false])
+        let result = json.description
+        NSLog(result)
+        //let data = result.data(using: String.Encoding.utf8)!
+    }
+    
+    @IBAction func moveSteeringButton(_ sender: UIButton) {
+        let json = JSON(["type": "Move Steering", "steering": 0, "power": 50, "rotations": 5, "brake": false])
+        let result = json.description
+        NSLog(result)
+        //let data = result.data(using: String.Encoding.utf8)!
+    }
 
+    @IBAction func moveTankButton(_ sender: UIButton) {
+        let json = JSON(["type": "Move Steering", "powerLeft": 50, "powerRight": 50, "rotations": 5, "brake": false])
+        let result = json.description
+        NSLog(result)
+        //let data = result.data(using: String.Encoding.utf8)!
+    }
+    
+    @IBAction func displayButton(_ sender: UIButton) {
+        let json = JSON(["type": "Display", "clearScreen": true, "x": 0, "y": 0])
+        let result = json.description
+        NSLog(result)
+        //let data = result.data(using: String.Encoding.utf8)!
+    }
+    
+    @IBAction func soundButton(_ sender: UIButton) {
+        let json = JSON(["type": "Sound", "volume": 100, "playType": "repeat"])
+        let result = json.description
+        NSLog(result)
+        //let data = result.data(using: String.Encoding.utf8)!
+    }
+    
+    
 }
 
