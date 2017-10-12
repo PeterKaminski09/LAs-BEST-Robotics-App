1afa6587f1bbdefc5851c07fff3214556635b578
alinasun <alinasun@usc.edu>
2017-10-12 16:23:44 -0700

    UIdrag and drop

--- NXT_Programming/ViewController.swift
+++ NXT_Programming/ViewController.swift
@@ -10,9 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var mediumMotorButtonUI: UIButton!
+    @IBOutlet weak var largeMotorButtonUI: UIButton!
+    @IBOutlet weak var moveSteeringButtonUI: UIButton!
+    @IBOutlet weak var moveTankButtonUI: UIButton!
+    @IBOutlet weak var soundButtonUI: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        mediumMotorButtonUI.addTarget(self,
+                                      action: #selector(dragMM(control:event:)),
+                         for: UIControlEvents.touchDragInside)
+        mediumMotorButtonUI.addTarget(self,
+                                      action: #selector(dragMM(control:event:)),
+                         for: [UIControlEvents.touchDragExit,
+                               UIControlEvents.touchDragOutside])
+        
+        largeMotorButtonUI.addTarget(self,
+                                      action: #selector(dragLM(control:event:)),
+                                      for: UIControlEvents.touchDragInside)
+        largeMotorButtonUI.addTarget(self,
+                                      action: #selector(dragLM(control:event:)),
+                                      for: [UIControlEvents.touchDragExit,
+                                            UIControlEvents.touchDragOutside])
+        
+        moveSteeringButtonUI.addTarget(self,
+                                     action: #selector(dragMS(control:event:)),
+                                     for: UIControlEvents.touchDragInside)
+        moveSteeringButtonUI.addTarget(self,
+                                     action: #selector(dragMS(control:event:)),
+                                     for: [UIControlEvents.touchDragExit,
+                                           UIControlEvents.touchDragOutside])
+
+        moveTankButtonUI.addTarget(self,
+                                       action: #selector(dragMT(control:event:)),
+                                       for: UIControlEvents.touchDragInside)
+        moveTankButtonUI.addTarget(self,
+                                       action: #selector(dragMT(control:event:)),
+                                       for: [UIControlEvents.touchDragExit,
+                                             UIControlEvents.touchDragOutside])
+        
+        soundButtonUI.addTarget(self,
+                                       action: #selector(dragS(control:event:)),
+                                       for: UIControlEvents.touchDragInside)
+        soundButtonUI.addTarget(self,
+                                       action: #selector(dragS(control:event:)),
+                                       for: [UIControlEvents.touchDragExit,
+                                             UIControlEvents.touchDragOutside])
     }
     
     override func didReceiveMemoryWarning() {
@@ -65,5 +111,60 @@ class ViewController: UIViewController {
         let clientController = ClientController()
         clientController.sendDataToServerWith(jsonData: json.description)
     }
+    
+    func replaceDraggedButton(type: String, _x: Int, _y: Int) {
+        let button = UIButton()
+        button.frame = CGRect(x: _x, y: _y, width: 125, height: 100)
+        button.setTitle(type, for: UIControlState())
+        button.addTarget(self, action: #selector(dragMM(control:event:)), for: UIControlEvents.touchDragInside)
+        button.layer.borderColor = UIColor.yellow.cgColor
+        button.backgroundColor = UIColor.blue
+        button.layer.borderWidth = 1.2
+        button.layer.cornerRadius = 5
+        button.layer.masksToBounds = true
+        button.setTitleColor(UIColor.white, for: UIControlState.normal)
+        button.tag = 1
+        self.view.addSubview(button)
+    }
+    
+    func dragMM(control: UIControl, event: UIEvent) {
+        if let center = event.allTouches?.first?.location(in: self.view) {
+            control.center = center
+        }
+        replaceDraggedButton(type: "Medium Motor", _x: 20, _y: 648)
+        mediumMotorButton(mediumMotorButtonUI)
+    }
+    
+    func dragLM(control: UIControl, event: UIEvent) {
+        if let center = event.allTouches?.first?.location(in: self.view) {
+            control.center = center
+        }
+        replaceDraggedButton(type: "Large Motor", _x: 158, _y: 648)
+        largeMotorButton(largeMotorButtonUI)
+    }
+    
+    func dragMS(control: UIControl, event: UIEvent) {
+        if let center = event.allTouches?.first?.location(in: self.view) {
+            control.center = center
+        }
+        replaceDraggedButton(type: "Move Steering", _x: 286, _y: 648)
+        moveSteeringButton(moveSteeringButtonUI)
+    }
+    
+    func dragMT(control: UIControl, event: UIEvent) {
+        if let center = event.allTouches?.first?.location(in: self.view) {
+            control.center = center
+        }
+        replaceDraggedButton(type: "Move Tank", _x: 419, _y: 648)
+        moveTankButton(moveSteeringButtonUI)
+    }
+    func dragS(control: UIControl, event: UIEvent) {
+        if let center = event.allTouches?.first?.location(in: self.view) {
+            control.center = center
+        }
+        replaceDraggedButton(type: "Sound", _x: 685, _y: 648)
+        soundButton(soundButtonUI)
+    }
+    
 }
 
